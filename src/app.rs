c6c221e66bc95ad49cf047ecd1ace92fc6c1ec8d
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2023-07-27 21:25:44 -0300

    Implement counters
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use crate::brain::Brain;
+use crate::brain::*;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -66,54 +66,30 @@ impl eframe::App for MyApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Nosotros");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            if ui.button("Increment").clicked() {
+            if ui.button("+").clicked() {
                 self.brain.update(Event::IncrementA);
             }
-            if ui.button("Decrement").clicked() {
+            if ui.button("-").clicked() {
                 self.brain.update(Event::DecrementA);
             }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
+            ui.heading(self.brain.state.counterA.to_string());
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("Elles");
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+            if ui.button("+").clicked() {
+                self.brain.update(Event::IncrementB);
+            }
+            if ui.button("-").clicked() {
+                self.brain.update(Event::DecrementB);
+            }
+            ui.heading(self.brain.state.counterB.to_string());
+
             egui::warn_if_debug_build(ui);
         });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
     }
 }
--- src/brain.rs
+++ src/brain.rs
@@ -1,23 +1,56 @@
+use std::ops::Sub;
+
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct Brain {
-    pub counterA: u8,
-    // pub store: Vec<Event>,
+    pub state: State,
+    pub state_history: Vec<State>,
 }
 
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+pub struct State {
+    pub counterA: u8,
+    pub counterB: u8,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            counterA: 0,
+            counterB: 0,
+        }
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
 pub enum Event {
     IncrementA,
     DecrementA,
+    IncrementB,
+    DecrementB,
+    Commit,
+    Rollback,
 }
 
 impl Brain {
     pub fn new() -> Self {
-        Self { counterA: 0 }
+        Self {
+            state: State::new(),
+            state_history: Vec::new(),
+        }
     }
 
     pub fn update(&mut self, event: Event) {
         match event {
-            Event::IncrementA => self.counterA += 1,
-            Event::DecrementA => self.counterA -= 1,
+            Event::IncrementA => self.state.counterA += 1,
+            Event::DecrementA => self.state.counterA = self.state.counterA.saturating_sub(1),
+            Event::IncrementB => self.state.counterB += 1,
+            Event::DecrementB => self.state.counterB = self.state.counterB.saturating_sub(1),
+            Event::Commit => self.state_history.push(self.state.clone()),
+            Event::Rollback => {
+                if let Some(state) = self.state_history.pop() {
+                    self.state = state;
+                }
+            }
         }
     }
 }
