2e535b97b0d901830692ae6e1eba1d0598fa5469
AgustinRamiroDiaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2023-07-30 21:33:37 +0000

    Deploying to gh-pages from @ AgustinRamiroDiaz/anotador-de-truco@cb3e342bbf5cf6817f72d24cdda19686a0b77088 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'anotador-de-truco';
+var filesToCache = [
+  './',
+  './index.html',
+  './anotador-de-truco.js',
+  './anotador-de-truco_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
